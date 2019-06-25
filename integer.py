9627e73b22b4ea5c8c8f41e33eadf7dd56365e32
Alagupreethi <51926024+Alagupreethi@users.noreply.github.com>
2019-06-25 16:34:31 +0530

    Add files via upload

--- integer.py
+++ integer.py
@@ -1,6 +1,7 @@
 k,n=map(int,input().split())
-p=list(map(int,input().split()))
-sum=0
+a=list(map(int,input().split()))
+s=0
 for i in range(n):
-    sum=sum+p[i]
-print(sum)  
+    s+=a[i]
+print(s)
+       
